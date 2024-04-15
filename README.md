41367c8437bfb6799e0400fa530dca2e929480d3
David Amram <163440706+davidamram423@users.noreply.github.com>
2024-04-15 14:51:05 -0400

    Final Commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Drum-Kit
+
 A website where you can play the drums using letters on your keyboard
+
+This project was made in order to further practice javascript functions.
---
+++ index.js
@@ -0,0 +1,80 @@
+//Detecting button press
+
+var numberOfButtons = document.querySelectorAll(".drum").length;
+
+for(var i = 0; i<numberOfButtons; i++){
+document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+
+    var buttonInnerHTML = this.innerHTML;
+
+    makeSound(buttonInnerHTML);
+
+    buttonAnimation(buttonInnerHTML);
+
+});
+    
+}
+
+//Detecting keyboard press
+
+document.addEventListener("keypress", function(event) {
+    
+    makeSound(event.key);
+
+    buttonAnimation(event.key);
+})
+
+function makeSound(key){
+
+    switch (key) {
+        case "a":
+                var crash = new Audio('sounds/crash.mp3');
+                crash.play(); //method to play back our media
+            break;
+    
+        case "s":
+                var kick = new Audio("sounds/kick-bass.mp3");
+                kick.play(); 
+            break;
+
+        case "d":
+                var snare = new Audio("sounds/snare.mp3");
+                snare.play(); 
+            break;
+
+        case "f":
+                var tom1 = new Audio("sounds/tom-1.mp3");
+                tom1.play(); 
+            break;
+
+        case "j":
+                var tom2 = new Audio("sounds/tom-2.mp3");
+                tom2.play(); 
+            break;
+
+        case "k":
+                var tom3 = new Audio("sounds/tom-3.mp3");
+                tom3.play(); 
+            break;
+
+        case "l":
+                var tom4 = new Audio("sounds/tom-4.mp3");
+                tom4.play(); 
+            break;
+
+        default: console.log(buttonInnerHTML);
+    }
+}
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+
+    activeButton.classList.add("pressed");
+
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    }, 100);
+}
+
+
+//ADD COMMENTSSTSTSTTSTS
